3763af8bacaf7e3056caeda5c957d72b3a83247b
Uma22saraswathi <umasaraswathi22@gmail.com>
2024-10-05 11:03:20 +0530

    abcd

---
+++ java script/new.js
@@ -0,0 +1,31 @@
+console.log("hello world")
+alert("welcome to my page")
+var
+a=4
+b=5
+console.log(a+b)
+console.table({fname:"uma",age:19})
+console.error("hello");
+console.warn("danger")
+for(i=0;i<10;i++);
+{console.log(i)}
+
+console.log(typeof("uma"));
+console.log(typeof(34567));
+var a=10
+console.log(a)
+var b=11;
+console.log(b)
+console.log((a==b))
+var z;
+console.log(z);
+{
+    let a=10
+    console.log(a)
+}
+{
+    var a=10
+    console.log(a)
+}
+
+ 
